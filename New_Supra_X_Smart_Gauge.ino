4565eb8ba9addc78888e8bb9fc726b89fa687502
garin_prodigi <gerin.rin@gmail.com>
2024-02-13 20:13:57 +0700

    update sprite logic

--- New_Supra_X_Smart_Gauge.ino
+++ New_Supra_X_Smart_Gauge.ino
@@ -13,18 +13,24 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, 25200L);  // 25200L is 25200 second offset for GMT+7
 
 void setup() {
+    Serial.begin(115200);
+
     // Connect to WiFi
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.println("Connecting to WiFi..");
     }
+
     Serial.println("Connected to WiFi");
 
     // Initialize devices
     initScreen();        // Screen
     timeClient.begin();  // NTP client
 }
+
+int temp = 0;
+int alt = 0;
 
 void loop() {
     // Get current time
@@ -33,8 +39,17 @@ void loop() {
     int minutes = timeClient.getMinutes();
 
     // Update screen
+    updateRingGauge(temp, alt);
     updateClock(hours, minutes);
-    updateRingGauge(360);
 
-    delay(1000);  // Update every second
+    temp += 1;
+    alt += 50;
+
+    if (temp >= 120) {
+        temp = 0;
+    }
+    if (alt >= 1500) {
+        alt = 0;
+    }
+    delay(1000);
 }
--- internal/screen/screen.cpp
+++ internal/screen/screen.cpp
@@ -17,7 +17,7 @@ int maxCircleRadius = 120;                  // Max circle radius from mid to edg
 
 // Sprite frame size configuration
 int spriteClockSize[] = {140, 50};                        // Width x Height
-int spriteRingGaugeSize[] = {screenWidth, screenHeight};  // Width x Height same as main screen size
+int spriteRingGaugeSize[] = {screenWidth, screenHeight};  // Width x Height
 
 void initScreen(void) {
     // Init display
@@ -26,23 +26,45 @@ void initScreen(void) {
     mainScreen.setRotation(2);         // 2 = Flip vertically
 
     // Creating new sprite frame
-    spriteClock.createSprite(spriteClockSize[0], spriteClockSize[1]);              // Clock
-    ringGaugeSprite.createSprite(spriteRingGaugeSize[0], spriteRingGaugeSize[1]);  // Ring gauge
+    // Clock
+    ringGaugeSprite.setColorDepth(8);
+    spriteClock.createSprite(spriteClockSize[0], spriteClockSize[1]);
+    // Ring gauge
+    ringGaugeSprite.setColorDepth(8);
+    ringGaugeSprite.createSprite(spriteRingGaugeSize[0], spriteRingGaugeSize[1]);
 }
 
 // updateRingGauge receive temperature value
-void updateRingGauge(int temperature) {
+void updateRingGauge(int temperature, int altitude) {
     // Reset content on the screen
-    ringGaugeSprite.fillScreen(TFT_BLUE);
+    ringGaugeSprite.fillScreen(TFT_TRANSPARENT);
 
-    // Draw Arc (Lingkaran Cincin)
-    ringGaugeSprite.drawArc(midScreenCoordinate, midScreenCoordinate, maxCircleRadius, 50, 0, temperature, TFT_WHITE, TFT_WHITE, true);
+    /* Attention
+        1. Start radius and end radius cannot be same
+        2. Ketika remap value temperature start radius di tambah 1 agar tidak sama dengan end radius ketika temperature = 0
+    */
+    int ringThickness = maxCircleRadius - 10;  // Ketebalan 10px
+
+    // Draw Arc (Lingkaran Cincin Kiri)
+    // 0 start di jam 6 || 180 di jam 12
+    int startRadius = 45;                                                   // Jam 7:30
+    int endRadius = 135;                                                    // Jam 10:30
+    int gaugeValue = map(temperature, 0, 120, startRadius + 1, endRadius);  // Remap temperature value, 0 - 120C range to radius value
+    ringGaugeSprite.drawSmoothArc(midScreenCoordinate, midScreenCoordinate, maxCircleRadius, ringThickness, startRadius, gaugeValue, TFT_RED, TFT_RED, false);
+    Serial.printf("temperature and gauge %d %d \n", temperature, gaugeValue);
+
+    // Draw Arc (Lingkaran Cincin Kanan)
+    startRadius = 225;                                                // Jam 1:30
+    endRadius = 315;                                                  // Jam 4:30
+    gaugeValue = map(altitude, 1500, 0, startRadius, endRadius - 1);  // Remap altitude value 0 - 1500 MDPL range to radius value
+    ringGaugeSprite.drawSmoothArc(midScreenCoordinate, midScreenCoordinate, maxCircleRadius, ringThickness, gaugeValue, endRadius, TFT_BLUE, TFT_BLUE, false);
+    Serial.printf("alt and gauge %d %d \n", altitude, gaugeValue);
 
     // Display information to screen
     ringGaugeSprite.pushSprite(0, 0, TFT_TRANSPARENT);  // Display information to screen
 }
 
-// updateClock receive time with format HH:MM
+// updateClock receive hours and minutes
 void updateClock(int hours, int minutes) {
     // Format current time in "HH:MM" format and store it in formattedTime
     char formattedTime[5];
